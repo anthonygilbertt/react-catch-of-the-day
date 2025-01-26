e2a63564c9a3e933321402de7e17bb3e80ff0c48
Anthony Gilbert <anthony.gilbert93@gmail.com>
2025-01-26 02:47:51 -0600

    created a component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    // every class needs something called 'render'
+
+    render() {
+        return <p>Hello from StorePicker!</p> // we need to mound out app to the DOM
+    }
+}
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from 'react'
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker />, document.querySelector('#main')); //this takes 2 things
+
+// React can be used for iOS apps
+// React can be used for Android apps
+// React can be used to render to canvas
+// React can be used to render to the DOM
