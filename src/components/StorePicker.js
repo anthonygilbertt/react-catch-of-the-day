0811b103f39f461eeb366cbe977b1909e0fad640
Anthony Gilbert <anthony.gilbert93@gmail.com>
2025-01-26 03:14:41 -0600

    created a simple form that has no functionality

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,7 +4,18 @@ class StorePicker extends React.Component {
     // every class needs something called 'render'
 
     render() {
-        return <p>Hello from StorePicker!</p> // we need to mound out app to the DOM
+        // return <p>Hello from StorePicker!</p> 
+        return (
+            <>
+                <p>boop</p>
+                <form className="store-selector">
+                    { /* this is javascript land */ }
+                    <h2>Please Enter A Store</h2>
+                    <input type="text" required placeholder='Store Name' />
+                    <button type="submit">Visit Store -></button>
+                </form>
+            </>
+        )
     }
 }
 
